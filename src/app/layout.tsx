50e1536b7860ada467c8a4d27ee281c9d1521792
Maze Winther <mazewinther@gmail.com>
2025-04-07 22:32:50 +0200

    refactor: move pages into a (main) folder and add a layout.tsx file here with the header and footer
    ensures the header and footer don't show on pages like not found

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -112,9 +110,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Header />
           {children}
-          <Footer />
           <Analytics />
         </ThemeProvider>
       </body>
